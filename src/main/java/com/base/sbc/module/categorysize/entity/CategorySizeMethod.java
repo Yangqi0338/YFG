fe1105a8e6553d832ba239e34508f2d8103d4d31
卞康 <247967116@qq.com>
2023-04-06 15:03:55 +0800

    配置后端返回字段如果为null则转为空字符串

--- src/main/java/com/base/sbc/module/categorysize/entity/CategorySizeMethod.java
+++ src/main/java/com/base/sbc/module/categorysize/entity/CategorySizeMethod.java
@@ -7,43 +7,29 @@
 package com.base.sbc.module.categorysize.entity;
 
 import com.base.sbc.config.common.base.BaseDataEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
-/** 
+/**
  * 类描述：品类尺寸量法 实体类
  * @address com.base.sbc.basedata.entity.CategorySizeMethod
  * @author youkehai
  * @email 717407966@qq.com
  * @date 创建时间：2021-5-6 16:34:43
- * @version 1.0  
+ * @version 1.0
  */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class CategorySizeMethod extends BaseDataEntity<String> implements Cloneable{
 
 	private static final long serialVersionUID = 1L;
-	/**********************************实体存放的其他字段区  不替换的区域 【other_start】******************************************/
+
 	private List<String> sizeList;
 	private List<String> standardList;
 
-	public List<String> getSizeList() {
-		return sizeList;
-	}
-
-	public void setSizeList(List<String> sizeList) {
-		this.sizeList = sizeList;
-	}
-
-	public List<String> getStandardList() {
-		return standardList;
-	}
-
-	public void setStandardList(List<String> standardList) {
-		this.standardList = standardList;
-	}
-
-	/**********************************实体存放的其他字段区 【other_end】******************************************/
-	
-    /*****************************数据库字段区 不包含父类公共字段(属性)***********************************/
     /** 品类名称（对应CCM的品类名称） */
     private String categoryName;
     /** 部位名称 */
@@ -64,71 +50,5 @@ public class CategorySizeMethod extends BaseDataEntity<String> implements Clonea
     private String codeError;
     /** 基码 */
     private String baseSize;
-    /*******************************************getset方法区************************************/
-    public String getCategoryName() {
-		return categoryName;
-	}
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
-    public String getPartName() {
-		return partName;
-	}
-	public void setPartName(String partName) {
-		this.partName = partName;
-	}
-    public String getMethod() {
-		return method;
-	}
-	public void setMethod(String method) {
-		this.method = method;
-	}
-    public String getSize() {
-		return size;
-	}
-	public void setSize(String size) {
-		this.size = size;
-	}
-    public String getTolerance() {
-		return tolerance;
-	}
-	public void setTolerance(String tolerance) {
-		this.tolerance = tolerance;
-	}
-    public String getStandard() {
-		return standard;
-	}
-	public void setStandard(String standard) {
-		this.standard = standard;
-	}
-    public String getTips() {
-		return tips;
-	}
-	public void setTips(String tips) {
-		this.tips = tips;
-	}
-    public String getImage() {
-		return image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-    public String getCodeError() {
-		return codeError;
-	}
-	public void setCodeError(String codeError) {
-		this.codeError = codeError;
-	}
-    public String getBaseSize() {
-		return baseSize;
-	}
-	public void setBaseSize(String baseSize) {
-		this.baseSize = baseSize;
-	}
-
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		return super.clone();
-	}
 }
 
