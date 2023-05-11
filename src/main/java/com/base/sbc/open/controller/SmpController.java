5d3612aead53113bdaeff616ad4dd2cc833404ff
卞康 <247967116@qq.com>
2023-05-11 14:58:09 +0800

    增加数据获取开放接口

--- src/main/java/com/base/sbc/open/controller/SmpController.java
+++ src/main/java/com/base/sbc/open/controller/SmpController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = BaseController.OPEN_URL + "/smp", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public class SmpSupplierController extends BaseController {
+public class SmpController extends BaseController {
 
     /**
      * bp供应商
