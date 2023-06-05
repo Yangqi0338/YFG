1705527ccf15beb57c3ad649e76b8bb1d5ba0fde
卞康 <247967116@qq.com>
2023-06-05 16:09:03 +0800

    修改工艺资料库接口

--- src/main/java/com/base/sbc/module/basicsdatum/controller/ProcessDatabaseController.java
+++ src/main/java/com/base/sbc/module/basicsdatum/controller/ProcessDatabaseController.java
@@ -2,10 +2,12 @@ package com.base.sbc.module.basicsdatum.controller;
 
 import com.base.sbc.config.common.ApiResult;
 import com.base.sbc.config.common.base.BaseController;
+import com.base.sbc.module.basicsdatum.dto.ProcessDatabasePageDto;
+import com.base.sbc.module.basicsdatum.entity.ProcessDatabase;
 import com.base.sbc.module.basicsdatum.service.ProcessDatabaseService;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
 
 
 /**
@@ -35,8 +39,45 @@ public class ProcessDatabaseController extends BaseController {
     @ApiOperation(value = "导入工艺资料库")
     @PostMapping("/importExcel")
     public ApiResult importExcel(@RequestParam("file") MultipartFile file) throws Exception {
-       Boolean b=  processDatabaseService.importExcel(file);
-       return insertSuccess(b);
+        Boolean b = processDatabaseService.importExcel(file);
+        return insertSuccess(b);
+    }
+
+    /**
+     * 根据列表进行分页查询
+     */
+    @ApiOperation(value = "导入工艺资料库")
+    @PostMapping("/queryPage")
+    public ApiResult listPage(ProcessDatabasePageDto pageDto) throws Exception {
+        PageInfo<ProcessDatabase> pageInfo= processDatabaseService.listPage(pageDto);
+        return insertSuccess(pageInfo);
+    }
+
+    /**
+     * 新增或者修改
+     */
+    @ApiOperation(value = "新增或者修改工艺资料库")
+    @PostMapping("/save")
+    public ApiResult save(ProcessDatabasePageDto pageDto) throws Exception {
+        PageInfo<ProcessDatabase> pageInfo= processDatabaseService.listPage(pageDto);
+        return insertSuccess(pageInfo);
+    }
+
+    /**
+     * 根据id删除
+     */
+    @ApiOperation(value = "根据id删除")
+    @PostMapping("/delById")
+    public ApiResult delById(String id) throws Exception {
+        return deleteSuccess(processDatabaseService.removeById(id));
+    }
+    /**
+     * 根据id数组批量删除删除
+     */
+    @ApiOperation(value = "根据id数组批量删除删除")
+    @PostMapping("/delByIds")
+    public ApiResult delById(String[] ids) throws Exception {
+        return deleteSuccess(processDatabaseService.removeBatchByIds(Arrays.asList(ids)));
     }
 
 
---
+++ src/main/java/com/base/sbc/module/basicsdatum/dto/ProcessDatabasePageDto.java
@@ -0,0 +1,14 @@
+package com.base.sbc.module.basicsdatum.dto;
+
+import com.base.sbc.config.common.base.Page;
+import lombok.Data;
+
+/**
+ * @author 卞康
+ * @date 2023/6/5 11:48:13
+ * @mail 247967116@qq.com
+ */
+@Data
+public class ProcessDatabasePageDto extends Page {
+
+}
