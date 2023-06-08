63d613f4f620853d4394affb2e3ef7f9fabb59bb
卞康 <247967116@qq.com>
2023-06-08 17:22:45 +0800

    修改对接字段

--- src/main/java/com/base/sbc/module/smp/entity/TagPrinting.java
+++ src/main/java/com/base/sbc/module/smp/entity/TagPrinting.java
@@ -6,113 +6,240 @@ import java.util.List;
 
 @Data
 public class TagPrinting {
-    /** 唯一码 */
-    private String colorwayWareCode;
-    /** 是否赠品 */
-    private String isGift;
-    /** 颜色名称 */
-    private String colorDescription;
-    /** 颜色编码 */
-    private String colorCode;
-    /** 大货款号 */
-    private String styleCode;
-    /** 批次 */
-    private String colorwayBatchNo;
-    /** 商品吊牌价确认 */
-    private Boolean merchApproved;
-    /** 配饰款号 */
-    private String secCode;
-    /** 主款款号 */
-    private String mainCode;
-    /** 吊牌价 */
-    private String colorwaySalesPrice;
-    /** 是否内配饰 */
-    private Boolean isAccessories;
-    /** 系列 */
-    private String colorwaySeries;
-    /** 大货款号是否激活 */
-    private Boolean active;
-    /** 销售类型 */
-    private String colorwaySaleType;
-    /** 品牌_描述 */
-    private String seasonBrand;
-    /** 品类id */
-    private String collectionProdCategory;
-    /** 主题 */
-    private String theme;
-    /** 小类编码 */
-    private String style3rdCategory;
-    /** 中类编码 */
-    private String style2ndCategory;
-    /** 尺码号型编码 */
-    private String sizeRangeCode;
-    /** 尺码号型名称 */
-    private String sizeRangeName;
-    /** 款式分类 */
-    private String productType;
-    /** 大类 */
-    private String firstProdCategory;
-    /** 尺码号型:分类 */
-    private String sizeRangeDimensionType;
-    /** 成分 */
-    private String composition;
-    /** 洗标 */
-    private String careSymbols;
-    /** 质量登记 */
-    private String qualityClass;
-    /** 品名 */
-    private String productName;
-    /** 安全类别 */
-    private String saftyType;
-    /** 执行标准 */
-    private String opStandard;
-    /** 品控部确认 */
-    private Boolean approved;
-    /** 温馨提示 */
-    private String attention;
-    /** 后技术确认 */
-    private Boolean techApproved;
-    /** 安全标题 */
-    private String saftyTitle;
-    /** 洗唛材质备注 */
-    private String appBomComment;
-    /** 贮藏要求 */
-    private String appBomStorageReq;
-    /** 入库时间 */
-    private String appBomStorageTime;
-    /** 产地 */
-    private String appBomMadeIn;
-    /** 英文成分 */
-    private String compsitionMix;
-    /** 英文温馨提示 */
-    private String warmPointEn;
-    /** 英文贮藏要求 */
-    private String storageReqEn;
-    /** 尺码信息 */
+    /**
+     * 唯一码
+     */
+    private String C8_Colorway_WareCode;
+
+    /**
+     * 是否赠品
+     */
+    private Boolean IsGift;
+
+    /**
+     * 颜色名称
+     */
+    private String ColorDescription;
+
+    /**
+     * 颜色编码
+     */
+    private String ColorCode;
+
+    /**
+     * 大货款号
+     */
+    private String StyleCode;
+
+    /**
+     * 批次
+     */
+    private String C8_Colorway_BatchNo;
+
+    /**
+     * 商品吊牌价确认
+     */
+    private Boolean MerchApproved;
+
+    /**
+     * 配饰款号
+     */
+    private String SecCode;
+
+    /**
+     * 主款款号
+     */
+    private String MainCode;
+
+    /**
+     * 吊牌价
+     */
+    private String C8_Colorway_SalesPrice;
+
+    /**
+     * 是否内配饰
+     */
+    private Boolean IsAccessories;
+
+    /**
+     * 系列
+     */
+    private String C8_Colorway_Series;
+
+    /**
+     * 大货款号是否激活
+     */
+    private Boolean Active;
+
+    /**
+     * 销售类型
+     */
+    private String C8_Colorway_SaleType;
+
+    /**
+     * 品牌_描述
+     */
+    private String C8_Season_Brand;
+
+    /**
+     * 品类id
+     */
+    private String C8_Collection_ProdCategory;
+
+    /**
+     * 主题
+     */
+    private String Theme;
+
+    /**
+     * 小类编码
+     */
+    private String C8_Style_3rdCategory;
+
+    /**
+     * 中类编码
+     */
+    private String C8_Style_2ndCategory;
+
+    /**
+     * 尺码号型编码
+     */
+    private String SizeRangeCode;
+
+    /**
+     * 尺码号型名称
+     */
+    private String SizeRangeName;
+
+    /**
+     * 款式分类
+     */
+    private String ProductType;
+
+    /**
+     * 大类
+     */
+    private String C8_1stProdCategory;
+
+    /**
+     * 尺码号型:分类
+     */
+    private String SizeRangeDimensionType;
+
+    /**
+     * 成分
+     */
+    private String Composition;
+
+    /**
+     * 洗标
+     */
+    private String CareSymbols;
+
+    /**
+     * 质量登记
+     */
+    private String QualityClass;
+
+    /**
+     * 品名
+     */
+    private String ProductName;
+
+    /**
+     * 安全类别
+     */
+    private String SaftyType;
+
+    /**
+     * 执行标准
+     */
+    private String OPStandard;
+
+    /**
+     * 品控部确认
+     */
+    private String Approved;
+
+    /**
+     * 温馨提示
+     */
+    private String Attention;
+
+    /**
+     * 后技术确认
+     */
+    private String TechApproved;
+
+    /**
+     * 安全标题
+     */
+    private String SaftyTitle;
+
+    /**
+     * 洗唛材质备注
+     */
+    private String C8_APPBOM_Comment;
+
+    /**
+     * 贮藏要求
+     */
+    private String StorageRequirement;
+
+    /**
+     * 款式尺码明细
+     */
     private List<Size> size;
 
     /**
-     * 尺码信息
+     * 尺码明细类
      */
     @Data
     public static class Size {
-        /** 尺码 */
-        private String sizeCode;
-        /** 代码 */
-        private String sortCode;
-        /** 号型 */
-        private String sizeName;
-        /** 尺码编号 */
-        private String sizeID;
-        /** External Size */
-        private String extSizeCode;
-        /** 显示尺码标识 */
-        private String showIntSize;
-        /** 欧码 */
-        private String europeCode;
-        /** 充绒量 */
-        private String skuFiller;
-        /** 特殊规格 */
-        private String specialSpec;
+        /**
+         * 尺码
+         */
+        private String SIZECODE;
+
+        /**
+         * 代码
+         */
+        private String SORTCODE;
+
+        /**
+         * 号型
+         */
+        private String SIZENAME;
+
+        /**
+         * 尺码编号
+         */
+        private String SizeID;
+
+        /**
+         * External Size
+         */
+        private String EXTSIZECODE;
+
+        /**
+         * 显示尺码标识
+         */
+        private boolean ShowIntSize;
+
+        /**
+         * 欧码
+         */
+        private String EuropeCode;
+
+        /**
+         * 充绒量
+         */
+        private String SKUFiller;
+
+        /**
+         * 特殊规格
+         */
+        private String SpecialSpec;
     }
+
 }
--- src/main/java/com/base/sbc/open/controller/SmpController.java
+++ src/main/java/com/base/sbc/open/controller/SmpController.java
@@ -115,140 +115,62 @@ public class SmpController extends BaseController {
     @GetMapping("/TagPrinting")
     @ApiOperation(value = "吊牌打印获取", notes = "吊牌打印获取")
     public ApiResult tagPrinting(String id, Boolean bl) {
-        List<TagPrinting> tagPrintings = new ArrayList<>();
         TagPrinting tagPrinting = new TagPrinting();
-        tagPrinting.setColorwayWareCode("ABC123");
-        tagPrinting.setIsGift("No");
-        tagPrinting.setColorDescription("Red");
+
+        tagPrinting.setC8_Colorway_WareCode("ABC123");
+        tagPrinting.setIsGift(false);
+        tagPrinting.setColorDescription("Blue");
         tagPrinting.setColorCode("001");
-        tagPrinting.setStyleCode("STYLE001");
-        tagPrinting.setColorwayBatchNo("BATCH001");
+        tagPrinting.setStyleCode("12345");
+        tagPrinting.setC8_Colorway_BatchNo("BATCH001");
         tagPrinting.setMerchApproved(true);
         tagPrinting.setSecCode("SEC001");
         tagPrinting.setMainCode("MAIN001");
-        tagPrinting.setColorwaySalesPrice("99.99");
-        tagPrinting.setIsAccessories(true);
-        tagPrinting.setColorwaySeries("Series A");
+        tagPrinting.setC8_Colorway_SalesPrice("49.99");
+        tagPrinting.setIsAccessories(false);
+        tagPrinting.setC8_Colorway_Series("Series A");
         tagPrinting.setActive(true);
-        tagPrinting.setColorwaySaleType("Online");
-        tagPrinting.setSeasonBrand("Brand A");
-        tagPrinting.setCollectionProdCategory("Category A");
-        tagPrinting.setTheme("Theme A");
-        tagPrinting.setStyle3rdCategory("Category 3");
-        tagPrinting.setStyle2ndCategory("Category 2");
-        tagPrinting.setSizeRangeCode("SR001");
-        tagPrinting.setSizeRangeName("Size Range A");
-        tagPrinting.setProductType("Type A");
-        tagPrinting.setFirstProdCategory("Category 1");
-        tagPrinting.setSizeRangeDimensionType("Type B");
+        tagPrinting.setC8_Colorway_SaleType("Retail");
+        tagPrinting.setC8_Season_Brand("Brand X");
+        tagPrinting.setC8_Collection_ProdCategory("123");
+        tagPrinting.setTheme("Summer");
+        tagPrinting.setC8_Style_3rdCategory("456");
+        tagPrinting.setC8_Style_2ndCategory("789");
+        tagPrinting.setSizeRangeCode("XL");
+        tagPrinting.setSizeRangeName("Extra Large");
+        tagPrinting.setProductType("T-Shirt");
+        tagPrinting.setC8_1stProdCategory("Clothing");
+        tagPrinting.setSizeRangeDimensionType("Dimensions");
         tagPrinting.setComposition("Cotton");
-        tagPrinting.setCareSymbols("Wash with care");
+        tagPrinting.setCareSymbols("Wash with cold water");
         tagPrinting.setQualityClass("Class A");
-        tagPrinting.setProductName("Product A");
-        tagPrinting.setSaftyType("Type A");
-        tagPrinting.setOpStandard("Standard A");
-        tagPrinting.setApproved(true);
-        tagPrinting.setAttention("Attention A");
-        tagPrinting.setTechApproved(true);
-        tagPrinting.setSaftyTitle("Title A");
-        tagPrinting.setAppBomComment("BOM Comment A");
+        tagPrinting.setProductName("T-Shirt");
+        tagPrinting.setSaftyType("Type 1");
+        tagPrinting.setOPStandard("Standard X");
+        tagPrinting.setApproved("Yes");
+        tagPrinting.setAttention("Handle with care");
+        tagPrinting.setTechApproved("Yes");
+        tagPrinting.setSaftyTitle("Safety First");
+        tagPrinting.setC8_APPBOM_Comment("Handle with care");
+        tagPrinting.setStorageRequirement("Dry place");
 
-        // 设置尺码信息
-        List<TagPrinting.Size> sizeInfo = new ArrayList<>();
-        TagPrinting.Size size1 = new TagPrinting.Size();
-        size1.setSizeCode("S");
-        size1.setSortCode("1");
-        size1.setSizeName("Small");
-        size1.setSizeID("S001");
-        size1.setExtSizeCode("ES001");
-        size1.setShowIntSize("Yes");
-        size1.setEuropeCode("EU001");
-        size1.setSkuFiller("100g");
-        size1.setSpecialSpec("None");
-        sizeInfo.add(size1);
+// 创建尺码明细对象
+        TagPrinting.Size size = new TagPrinting.Size();
+        size.setSIZECODE("XXXL");
+        size.setSORTCODE("0");
+        size.setSIZENAME("175/80A");
+        size.setSizeID("10270");
+        size.setEXTSIZECODE("");
+        size.setShowIntSize(true);
+        size.setEuropeCode("");
+        size.setSKUFiller("");
+        size.setSpecialSpec("");
 
-        TagPrinting.Size size2 = new TagPrinting.Size();
-        size2.setSizeCode("M");
-        size2.setSortCode("2");
-        size2.setSizeName("Medium");
-        size2.setSizeID("M001");
-        size2.setExtSizeCode("EM001");
-        size2.setShowIntSize("Yes");
-        size2.setEuropeCode("EU002");
-        size2.setSkuFiller("150g");
-        size2.setSpecialSpec("None");
-        sizeInfo.add(size2);
-        tagPrinting.setSize(sizeInfo);
-
-
-        TagPrinting tagPrinting2 = new TagPrinting();
-        tagPrinting2.setColorwayWareCode("XYZ789");
-        tagPrinting2.setIsGift("Yes");
-        tagPrinting2.setColorDescription("Blue");
-        tagPrinting2.setColorCode("002");
-        tagPrinting2.setStyleCode("STYLE002");
-        tagPrinting2.setColorwayBatchNo("BATCH002");
-        tagPrinting2.setMerchApproved(true);
-        tagPrinting2.setSecCode("SEC002");
-        tagPrinting2.setMainCode("MAIN002");
-        tagPrinting2.setColorwaySalesPrice("49.99");
-        tagPrinting2.setIsAccessories(true);
-        tagPrinting2.setColorwaySeries("Series B");
-        tagPrinting2.setActive(true);
-        tagPrinting2.setColorwaySaleType("Offline");
-        tagPrinting2.setSeasonBrand("Brand B");
-        tagPrinting2.setCollectionProdCategory("Category B");
-        tagPrinting2.setTheme("Theme B");
-        tagPrinting2.setStyle3rdCategory("Category 4");
-        tagPrinting2.setStyle2ndCategory("Category 3");
-        tagPrinting2.setSizeRangeCode("SR002");
-        tagPrinting2.setSizeRangeName("Size Range B");
-        tagPrinting2.setProductType("Type B");
-        tagPrinting2.setFirstProdCategory("Category 2");
-        tagPrinting2.setSizeRangeDimensionType("Type C");
-        tagPrinting2.setComposition("Polyester");
-        tagPrinting2.setCareSymbols("Dry clean only");
-        tagPrinting2.setQualityClass("Class B");
-        tagPrinting2.setProductName("Product B");
-        tagPrinting2.setSaftyType("Type B");
-        tagPrinting2.setOpStandard("Standard B");
-        tagPrinting2.setApproved(true);
-        tagPrinting2.setAttention("Attention B");
-        tagPrinting2.setTechApproved(true);
-        tagPrinting2.setSaftyTitle("Title B");
-        tagPrinting2.setAppBomComment("BOM Comment B");
-
-
-        // 设置尺码信息
-        List<TagPrinting.Size> sizeInfo2 = new ArrayList<>();
-        TagPrinting.Size size3 = new TagPrinting.Size();
-        size1.setSizeCode("L");
-        size1.setSortCode("3");
-        size1.setSizeName("Large");
-        size1.setSizeID("L002");
-        size1.setExtSizeCode("EL002");
-        size1.setShowIntSize("Yes");
-        size1.setEuropeCode("EU003");
-        size1.setSkuFiller("200g");
-        size1.setSpecialSpec("None");
-        sizeInfo2.add(size3);
-
-        TagPrinting.Size size4 = new TagPrinting.Size();
-        size2.setSizeCode("XL");
-        size2.setSortCode("4");
-        size2.setSizeName("Extra Large");
-        size2.setSizeID("XL002");
-        size2.setExtSizeCode("EXL002");
-        size2.setShowIntSize("Yes");
-        size2.setEuropeCode("EU004");
-        size2.setSkuFiller("250g");
-        size2.setSpecialSpec("None");
-        sizeInfo2.add(size4);
-
-        tagPrinting2.setSize(sizeInfo2);
-
+// 设置尺码明细
+        ArrayList<TagPrinting.Size> sizes =new ArrayList<>();
+        tagPrinting.setSize(sizes);
+        ArrayList<TagPrinting> tagPrintings = new ArrayList<>();
         tagPrintings.add(tagPrinting);
-        tagPrintings.add(tagPrinting2);
         return selectSuccess(tagPrintings);
     }
 }
