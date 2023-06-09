696d598e70c25cce0259bc404d59ea8523f2a7bf
孟繁江 <2915350015@qq.com>
2023-06-09 17:25:26 +0800

    提函数

---
+++ src/main/java/com/base/sbc/config/function/Function.java
@@ -0,0 +1,57 @@
+package com.base.sbc.config.function;
+
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.runtime.function.AbstractFunction;
+import com.googlecode.aviator.runtime.function.FunctionUtils;
+import com.googlecode.aviator.runtime.type.AviatorDouble;
+import com.googlecode.aviator.runtime.type.AviatorObject;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class Function  implements ApplicationRunner {
+
+
+    @Override
+    public void run(ApplicationArguments applicationArguments)  {
+        init();
+    }
+
+    public void init() {
+        /*sum函数*/
+        AviatorEvaluator.addFunction(new AbstractFunction() {
+            @Override
+            public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
+                Number left = FunctionUtils.getNumberValue(arg1, env);
+                Number right = FunctionUtils.getNumberValue(arg2, env);
+                return new AviatorDouble(left.doubleValue() + right.doubleValue());
+            }
+
+            @Override
+            public String getName() {
+                return "SUM";
+            }
+        });
+        /*if*/
+        AviatorEvaluator.addFunction(new AbstractFunction() {
+            @Override
+            public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
+                Number left = FunctionUtils.getNumberValue(arg1, env);
+                Number right = FunctionUtils.getNumberValue(arg2, env);
+                return new AviatorDouble(left.doubleValue() + right.doubleValue());
+            }
+
+            @Override
+            public String getName() {
+                return "IF";
+            }
+        });
+
+
+    }
+
+
+}
