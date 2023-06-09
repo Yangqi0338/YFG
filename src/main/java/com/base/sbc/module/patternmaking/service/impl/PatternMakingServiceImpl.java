ff2ae0c3ef264f999a4e69b652ccc9bac7c99c1b
lxl <li_xianglin@126.com>
2023-06-09 10:32:32 +0800

    bug fix

--- src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
+++ src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
@@ -174,7 +174,7 @@ public class PatternMakingServiceImpl extends ServicePlusImpl<PatternMakingMappe
         qw.eq("design_send_status", BaseGlobal.YES);
         qw.eq("s.del_flag", BaseGlobal.NO);
         qw.eq("p.del_flag", BaseGlobal.NO);
-        qw.orderBy(true, true, "p.create_date");
+        qw.last("order by p.create_date asc");
         Page<TechnologyCenterTaskVo> page = PageHelper.startPage(dto);
         List<TechnologyCenterTaskVo> list = getBaseMapper().technologyCenterTaskList(qw);
         //设置图片
