6fad5086f62b7afc39473b9fb05cf1f2df9714f4
孟繁江 <2915350015@qq.com>
2023-06-12 11:00:36 +0800

    调整自定义函数

--- src/main/java/com/base/sbc/config/function/Function.java
+++ src/main/java/com/base/sbc/config/function/Function.java
@@ -2,6 +2,7 @@ package com.base.sbc.config.function;
 
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.runtime.function.AbstractFunction;
+import com.googlecode.aviator.runtime.function.AbstractVariadicFunction;
 import com.googlecode.aviator.runtime.function.FunctionUtils;
 import com.googlecode.aviator.runtime.type.AviatorDouble;
 import com.googlecode.aviator.runtime.type.AviatorObject;
@@ -21,31 +22,62 @@ public class Function  implements ApplicationRunner {
     }
 
     public void init() {
-        /*sum函数*/
-        AviatorEvaluator.addFunction(new AbstractFunction() {
+        /*sum*/
+        AviatorEvaluator.addFunction(new AbstractVariadicFunction() {
             @Override
-            public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
-
-                Number left = FunctionUtils.getNumberValue(arg1, env);
-                Number right = FunctionUtils.getNumberValue(arg2, env);
-                return new AviatorDouble(left.doubleValue() + right.doubleValue());
+            public AviatorObject variadicCall(Map<String, Object> map, AviatorObject... args) {
+                Double sum = 0D;
+                for(AviatorObject arg : args){
+                    Number number = FunctionUtils.getNumberValue(arg, map);
+                    sum+=number.doubleValue();
+                }
+                return new AviatorDouble(sum);
             }
-
 
             @Override
             public String getName() {
                 return "SUM";
             }
         });
+
+        /*Avg*/
+        AviatorEvaluator.addFunction(new AbstractVariadicFunction() {
+            @Override
+            public AviatorObject variadicCall(Map<String, Object> map, AviatorObject... args) {
+                Double sum = 0D;
+                Integer count = 0;
+                for(AviatorObject arg:args){
+                    Number number = FunctionUtils.getNumberValue(arg, map);
+                    sum+=number.doubleValue();
+                    count++;
+                }
+                return new AviatorDouble(sum / count);
+            }
+
+            @Override
+            public String getName() {
+                return "Avg";
+            }
+        });
+
+
         /*if*/
         AviatorEvaluator.addFunction(new AbstractFunction() {
             @Override
-            public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
-                Number left = FunctionUtils.getNumberValue(arg1, env);
-                Number right = FunctionUtils.getNumberValue(arg2, env);
-                return new AviatorDouble(left.doubleValue() + right.doubleValue());
+            public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2, AviatorObject arg3) {
+                Boolean ifResult = FunctionUtils.getBooleanValue(arg1, env);
+                Number ifTrue = FunctionUtils.getNumberValue(arg2, env);
+                Number ifFalse = FunctionUtils.getNumberValue(arg3, env);
+                if (ifResult) {
+                    return new AviatorDouble(ifTrue.doubleValue());
+                } else {
+                    return new AviatorDouble(ifFalse.doubleValue());
+                }
             }
 
+            /**
+             * 返回方法名
+             */
             @Override
             public String getName() {
                 return "IF";
