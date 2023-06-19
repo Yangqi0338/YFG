26973918ca4f8d2c7e14d07e0d2afb554a7b532e
卞康 <247967116@qq.com>
2023-06-19 17:15:31 +0800

    增加操作日志

---
+++ src/main/java/com/base/sbc/module/operaLog/dto/OperaLogDto.java
@@ -0,0 +1,29 @@
+package com.base.sbc.module.operaLog.dto;
+
+import com.base.sbc.config.common.base.Page;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+/**
+ * @author 卞康
+ * @date 2023/6/19 16:55:50
+ * @mail 247967116@qq.com
+ */
+@Data
+public class OperaLogDto extends Page {
+    /**
+     * 单据id
+     */
+    private String documentId;
+    /**
+     * 操作类型
+     */
+    private String type;
+    /**
+     * 模块名称
+     */
+    private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private String[] createDate;
+}
