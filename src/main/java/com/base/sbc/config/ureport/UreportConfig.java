78af61c0959e02d95a2c4120f7780fb427ee58e2
szx <731139982@qq.com>
2023-06-20 15:58:15 +0800

    fix:注释Ureport尝试解决pdm注册nacos失败问题

--- src/main/java/com/base/sbc/config/ureport/UreportConfig.java
+++ src/main/java/com/base/sbc/config/ureport/UreportConfig.java
@@ -1,23 +1,15 @@
 package com.base.sbc.config.ureport;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-import com.bstek.ureport.console.UReportServlet;
-
-@Configuration
-@EnableAutoConfiguration
-@ImportResource("classpath:ureport-console-context.xml")
-public class UreportConfig {
-
-
-	@Bean
-	public ServletRegistrationBean buildUreportServlet() {
-		return new ServletRegistrationBean(new UReportServlet(), "/ureport/*");
-	}
-
-
-}
+//@Configuration
+//@EnableAutoConfiguration
+//@ImportResource("classpath:ureport-console-context.xml")
+//public class UreportConfig {
+//
+//
+//	@Bean
+//	public ServletRegistrationBean buildUreportServlet() {
+//		return new ServletRegistrationBean(new UReportServlet(), "/ureport/*");
+//	}
+//
+//
+//}
