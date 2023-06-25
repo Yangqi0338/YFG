cc769147b0c29e3088545c050922405d425f5b38
卞康 <247967116@qq.com>
2023-06-25 18:04:18 +0800

    增加restTemplate拦截器

--- src/main/java/com/base/sbc/config/restTemplate/RestTemplateService.java
+++ src/main/java/com/base/sbc/config/restTemplate/RestTemplateService.java
@@ -3,9 +3,11 @@ package com.base.sbc.config.restTemplate;
 
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSONObject;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -13,17 +15,18 @@ import org.springframework.web.client.RestTemplate;
  * @date 2023/5/8 15:31:18
  * @mail 247967116@qq.com
  */
+@Component
+@RequiredArgsConstructor
+public class RestTemplateService {
 
-public class RestTemplateUtils {
-    private static final RestTemplate restTemplate = new RestTemplate();
-
+    private final RestTemplate restTemplate;
     /**
      * smp系统对接post请求
      * @param url 请求地址
      * @param o 请求对象
      * @return 返回的结果
      */
-    public static Boolean spmPost(String url, Object o) {
+    public Boolean spmPost(String url, Object o) {
         HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.add("Content-Type", "application/json");
         // 2.请求头 & 请求体
