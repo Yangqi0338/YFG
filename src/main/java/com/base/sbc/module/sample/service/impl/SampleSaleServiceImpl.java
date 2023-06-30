97212b26be9666dceb8929994ed9928cacd6cd7b
panlinnuo <58615088+panlinnuo@users.noreply.github.com>
2023-06-30 11:10:54 +0800

    调整样衣销售列表、样衣销售新增接口

--- src/main/java/com/base/sbc/module/sample/service/impl/SampleSaleServiceImpl.java
+++ src/main/java/com/base/sbc/module/sample/service/impl/SampleSaleServiceImpl.java
@@ -68,17 +68,6 @@ public class SampleSaleServiceImpl extends BaseServiceImpl<SampleSaleMapper, Sam
 
             Integer count = 0, borrowCount = 0;
             for (SampleSaleItem item : dto.getSampleItemList()){
-                // 新增
-                if (StringUtil.isEmpty(item.getId())){
-                    item.setId(idGen.nextIdStr());
-                    item.setSampleSaleId(id);
-
-                    sampleSaleItemMapper.insert(item);
-                // 修改
-                } else {
-
-                }
-
                 // 处理样衣
                 sampleItemService.updateCount(item.getSampleItemId(), 3, item.getCount());
 
@@ -88,6 +77,7 @@ public class SampleSaleServiceImpl extends BaseServiceImpl<SampleSaleMapper, Sam
             }
 
             if (StringUtil.isEmpty(dto.getId())) {
+                sale.setCompanyCode(getCompanyCode());
                 mapper.insert(sale);
             } else {
                 mapper.updateById(sale);
@@ -113,9 +103,12 @@ public class SampleSaleServiceImpl extends BaseServiceImpl<SampleSaleMapper, Sam
     public PageInfo queryPageInfo(SampleSalePageDto dto) {
         QueryWrapper<SampleSaleVo> qw = new QueryWrapper<>();
         qw.eq("ss.company_code", getCompanyCode());
-        qw.ge(StrUtil.isNotEmpty(dto.getStartDate().toString()),"ss.sale_date", dto.getStartDate());
-        qw.le(StrUtil.isNotEmpty(dto.getEndDate().toString()),"ss.sale_date", dto.getEndDate());
-        qw.like(StrUtil.isNotBlank(dto.getSearch()), "s.design_no", dto.getSearch()).
+        if (null != dto.getStartDate())
+            qw.ge("ss.sale_date", dto.getStartDate());
+        if (null != dto.getEndDate())
+            qw.le("ss.sale_date", dto.getEndDate());
+        if (null != dto.getSearch())
+            qw.like("s.design_no", dto.getSearch()).
                 or().like(StrUtil.isNotBlank(dto.getSearch()), "si.code", dto.getSearch()).
                 or().like(StrUtil.isNotBlank(dto.getSearch()), "ss.code", dto.getSearch()).
                 or().like(StrUtil.isNotBlank(dto.getSearch()), "ss.custmer_name", dto.getSearch());
