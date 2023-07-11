76be828286ed3b5d7c4a1d105d3501870818a620
卞康 <247967116@qq.com>
2023-07-11 11:30:33 +0800

    修改吊牌打印

--- src/main/java/com/base/sbc/open/controller/SmpController.java
+++ src/main/java/com/base/sbc/open/controller/SmpController.java
@@ -27,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 卞康
@@ -44,8 +45,8 @@ public class SmpController extends BaseController {
     private final BasicsdatumSupplierService basicsdatumSupplierService;
 
     private final AmcService amcService;
-    @Autowired
-    private HangTagService hangTagService;
+
+    private final HangTagService hangTagService;
 
     /**
      * bp供应商
@@ -134,62 +135,65 @@ public class SmpController extends BaseController {
     @GetMapping("/TagPrinting")
     @ApiOperation(value = "吊牌打印获取", notes = "吊牌打印获取")
     public ApiResult tagPrinting(String id, Boolean bl) {
-        TagPrinting tagPrinting = new TagPrinting();
+        List<TagPrinting> tagPrintings1 = hangTagService.hangTagPrinting();
+        return selectSuccess(tagPrintings1);
 
-        tagPrinting.setC8_Colorway_WareCode("ABC123");
-        tagPrinting.setIsGift(false);
-        tagPrinting.setColorDescription("Blue");
-        tagPrinting.setColorCode("001");
-        tagPrinting.setStyleCode("12345");
-        tagPrinting.setC8_Colorway_BatchNo("BATCH001");
-        tagPrinting.setMerchApproved(true);
-        tagPrinting.setSecCode("SEC001");
-        tagPrinting.setMainCode("MAIN001");
-        tagPrinting.setC8_Colorway_SalesPrice("49.99");
-        tagPrinting.setIsAccessories(false);
-        tagPrinting.setC8_Colorway_Series("Series A");
-        tagPrinting.setActive(true);
-        tagPrinting.setC8_Colorway_SaleType("Retail");
-        tagPrinting.setC8_Season_Brand("Brand X");
-        tagPrinting.setC8_Collection_ProdCategory("123");
-        tagPrinting.setTheme("Summer");
-        tagPrinting.setC8_Style_3rdCategory("456");
-        tagPrinting.setC8_Style_2ndCategory("789");
-        tagPrinting.setSizeRangeCode("XL");
-        tagPrinting.setSizeRangeName("Extra Large");
-        tagPrinting.setProductType("T-Shirt");
-        tagPrinting.setC8_1stProdCategory("Clothing");
-        tagPrinting.setSizeRangeDimensionType("Dimensions");
-        tagPrinting.setComposition("Cotton");
-        tagPrinting.setCareSymbols("Wash with cold water");
-        tagPrinting.setQualityClass("Class A");
-        tagPrinting.setProductName("T-Shirt");
-        tagPrinting.setSaftyType("Type 1");
-        tagPrinting.setOPStandard("Standard X");
-        tagPrinting.setApproved(true);
-        tagPrinting.setAttention("Handle with care");
-        tagPrinting.setTechApproved(true);
-        tagPrinting.setSaftyTitle("Safety First");
-        tagPrinting.setC8_APPBOM_Comment("Handle with care");
-        tagPrinting.setStorageRequirement("Dry place");
-
-// 创建尺码明细对象
-        TagPrinting.Size size = new TagPrinting.Size();
-        size.setSIZECODE("XXXL");
-        size.setSORTCODE("0");
-        size.setSIZENAME("175/80A");
-        size.setSizeID("10270");
-        size.setEXTSIZECODE("");
-        size.setShowIntSize(true);
-        size.setEuropeCode("");
-        size.setSKUFiller("");
-        size.setSpecialSpec("");
-
-// 设置尺码明细
-        ArrayList<TagPrinting.Size> sizes =new ArrayList<>();
-        tagPrinting.setSize(sizes);
-        ArrayList<TagPrinting> tagPrintings = new ArrayList<>();
-        tagPrintings.add(tagPrinting);
-        return selectSuccess(tagPrintings);
+//        TagPrinting tagPrinting = new TagPrinting();
+//
+//        tagPrinting.setC8_Colorway_WareCode("ABC123");
+//        tagPrinting.setIsGift(false);
+//        tagPrinting.setColorDescription("Blue");
+//        tagPrinting.setColorCode("001");
+//        tagPrinting.setStyleCode("12345");
+//        tagPrinting.setC8_Colorway_BatchNo("BATCH001");
+//        tagPrinting.setMerchApproved(true);
+//        tagPrinting.setSecCode("SEC001");
+//        tagPrinting.setMainCode("MAIN001");
+//        tagPrinting.setC8_Colorway_SalesPrice("49.99");
+//        tagPrinting.setIsAccessories(false);
+//        tagPrinting.setC8_Colorway_Series("Series A");
+//        tagPrinting.setActive(true);
+//        tagPrinting.setC8_Colorway_SaleType("Retail");
+//        tagPrinting.setC8_Season_Brand("Brand X");
+//        tagPrinting.setC8_Collection_ProdCategory("123");
+//        tagPrinting.setTheme("Summer");
+//        tagPrinting.setC8_Style_3rdCategory("456");
+//        tagPrinting.setC8_Style_2ndCategory("789");
+//        tagPrinting.setSizeRangeCode("XL");
+//        tagPrinting.setSizeRangeName("Extra Large");
+//        tagPrinting.setProductType("T-Shirt");
+//        tagPrinting.setC8_1stProdCategory("Clothing");
+//        tagPrinting.setSizeRangeDimensionType("Dimensions");
+//        tagPrinting.setComposition("Cotton");
+//        tagPrinting.setCareSymbols("Wash with cold water");
+//        tagPrinting.setQualityClass("Class A");
+//        tagPrinting.setProductName("T-Shirt");
+//        tagPrinting.setSaftyType("Type 1");
+//        tagPrinting.setOPStandard("Standard X");
+//        tagPrinting.setApproved(true);
+//        tagPrinting.setAttention("Handle with care");
+//        tagPrinting.setTechApproved(true);
+//        tagPrinting.setSaftyTitle("Safety First");
+//        tagPrinting.setC8_APPBOM_Comment("Handle with care");
+//        tagPrinting.setStorageRequirement("Dry place");
+//
+//// 创建尺码明细对象
+//        TagPrinting.Size size = new TagPrinting.Size();
+//        size.setSIZECODE("XXXL");
+//        size.setSORTCODE("0");
+//        size.setSIZENAME("175/80A");
+//        size.setSizeID("10270");
+//        size.setEXTSIZECODE("");
+//        size.setShowIntSize(true);
+//        size.setEuropeCode("");
+//        size.setSKUFiller("");
+//        size.setSpecialSpec("");
+//
+//// 设置尺码明细
+//        ArrayList<TagPrinting.Size> sizes =new ArrayList<>();
+//        tagPrinting.setSize(sizes);
+//        ArrayList<TagPrinting> tagPrintings = new ArrayList<>();
+//        tagPrintings.add(tagPrinting);
+//        return selectSuccess(tagPrintings);
     }
 }
