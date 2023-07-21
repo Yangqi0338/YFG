9361f4faba730d2db146484b3e52d376001f5b65
卞康 <247967116@qq.com>
2023-07-21 11:05:27 +0800

    增加面料成分检测数据接口

--- src/main/java/com/base/sbc/open/mapper/EscmMaterialCompnentInspectCompanyMapper.java
+++ src/main/java/com/base/sbc/open/mapper/EscmMaterialCompnentInspectCompanyMapper.java
@@ -2,11 +2,13 @@ package com.base.sbc.open.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.base.sbc.open.entity.EscmMaterialCompnentInspectCompanyDto;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author 卞康
  * @date 2023/7/21 10:44:26
  * @mail 247967116@qq.com
  */
+@Mapper
 public interface EscmMaterialCompnentInspectCompanyMapper extends BaseMapper<EscmMaterialCompnentInspectCompanyDto> {
 }
--- src/main/java/com/base/sbc/open/service/EscmMaterialCompnentInspectCompanyService.java
+++ src/main/java/com/base/sbc/open/service/EscmMaterialCompnentInspectCompanyService.java
@@ -2,13 +2,11 @@ package com.base.sbc.open.service;
 
 import com.base.sbc.module.common.service.BaseService;
 import com.base.sbc.open.entity.EscmMaterialCompnentInspectCompanyDto;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author 卞康
  * @date 2023/7/21 10:44:18
  * @mail 247967116@qq.com
  */
-@Mapper
 public interface EscmMaterialCompnentInspectCompanyService extends BaseService<EscmMaterialCompnentInspectCompanyDto> {
 }
