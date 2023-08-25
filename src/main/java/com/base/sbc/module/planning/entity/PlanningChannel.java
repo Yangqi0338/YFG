0c44e9a2d430de6616d773d2bf641021be9f791e
tzy666 <974849633@qq.com>
2023-08-25 20:05:06 +0800

    企划渠道 增加字段 产品线

--- src/main/java/com/base/sbc/module/planning/entity/PlanningChannel.java
+++ src/main/java/com/base/sbc/module/planning/entity/PlanningChannel.java
@@ -5,22 +5,21 @@
  * 不得使用、复制、修改或发布本软件.
  *****************************************************************************/
 package com.base.sbc.module.planning.entity;
-
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.base.sbc.config.common.base.BaseDataEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
 /**
  * 类描述：企划-渠道 实体类
- *
- * @author your name
- * @version 1.0
  * @address com.base.sbc.module.planning.entity.PlanningChannel
- * @email your email
- * @date 创建时间：2023-7-21 17:59:40
+ * @author tzy
+ * @email 974849633@qq.com
+ * @date 创建时间：2023-8-25 20:02:35
+ * @version 1.0
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -28,68 +27,52 @@ import lombok.EqualsAndHashCode;
 @ApiModel("企划-渠道 PlanningChannel")
 public class PlanningChannel extends BaseDataEntity<String> {
 
-    private static final long serialVersionUID = 1L;
-    /**********************************实体存放的其他字段区  不替换的区域 【other_start】******************************************/
+	private static final long serialVersionUID = 1L;
+	/**********************************实体存放的其他字段区  不替换的区域 【other_start】******************************************/
 
 
     /**********************************实体存放的其他字段区 【other_end】******************************************/
 
     /*****************************数据库字段区 不包含父类公共字段(属性) 【start】***********************************/
-    /**
-     * 产品季节id
-     */
-    @ApiModelProperty(value = "产品季节id")
+    /** 产品季节id */
+    @ApiModelProperty(value = "产品季节id"  )
     private String planningSeasonId;
-    /**
-     * 品牌
-     */
-    @ApiModelProperty(value = "品牌")
+    /** 品牌 */
+    @ApiModelProperty(value = "品牌"  )
     private String brand;
-    /**
-     * 品牌名称
-     */
-    @ApiModelProperty(value = "品牌名称")
+    /** 品牌名称 */
+    @ApiModelProperty(value = "品牌名称"  )
     private String brandName;
-    /**
-     * 年份
-     */
-    @ApiModelProperty(value = "年份")
+    /** 年份 */
+    @ApiModelProperty(value = "年份"  )
     private String year;
-    /**
-     * 年份名称
-     */
-    @ApiModelProperty(value = "年份名称")
+    /** 年份名称 */
+    @ApiModelProperty(value = "年份名称"  )
     private String yearName;
-    /**
-     * 季节
-     */
-    @ApiModelProperty(value = "季节")
+    /** 季节 */
+    @ApiModelProperty(value = "季节"  )
     private String season;
-    /**
-     * 季节名称
-     */
-    @ApiModelProperty(value = "季节名称")
+    /** 季节名称 */
+    @ApiModelProperty(value = "季节名称"  )
     private String seasonName;
-    /**
-     * 渠道
-     */
-    @ApiModelProperty(value = "渠道")
+    /** 渠道 */
+    @ApiModelProperty(value = "渠道"  )
     private String channel;
-    /**
-     * 渠道名称
-     */
-    @ApiModelProperty(value = "渠道名称")
+    /** 渠道名称 */
+    @ApiModelProperty(value = "渠道名称"  )
     private String channelName;
-    /**
-     * 性别
-     */
-    @ApiModelProperty(value = "性别")
+    /** 性别 */
+    @ApiModelProperty(value = "性别"  )
     private String sex;
-    /**
-     * 性别名称
-     */
-    @ApiModelProperty(value = "性别名称")
+    /** 性别名称 */
+    @ApiModelProperty(value = "性别名称"  )
     private String sexName;
+    /** 产品线编码 */
+    @ApiModelProperty(value = "产品线编码"  )
+    private String productLine;
+    /** 产品线名称 */
+    @ApiModelProperty(value = "产品线名称"  )
+    private String productLineName;
     /*****************************数据库字段区 不包含父类公共字段(属性) 【end】 ***********************************/
 }
 
