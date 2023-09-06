83a860af156b26cc17b63be45791cf0895f419b0
lizan <2682766618@qq.com>
2023-09-06 14:57:46 +0800

    1、 修复生成SKU信息-设计款号为空

--- src/main/java/com/base/sbc/module/style/service/impl/StyleInfoColorServiceImpl.java
+++ src/main/java/com/base/sbc/module/style/service/impl/StyleInfoColorServiceImpl.java
@@ -230,7 +230,7 @@ public class StyleInfoColorServiceImpl extends BaseServiceImpl<StyleInfoColorMap
                     // 尺码名称
                     String sizeName =  null != productSizeList.get(i) ? productSizeList.get(i) : "";
                     // SKU ：颜色code + - + 尺码code
-                    String skuCode = packInfoDto.getDesignNo() + styleInfoColor.getColorCode() + sizeCode;
+                    String skuCode = styleDto.getDesignNo() + styleInfoColor.getColorCode() + sizeCode;
                     StyleInfoSku styleInfoSku = new StyleInfoSku();
                     styleInfoSku.setForeignId(styleInfoColor.getForeignId());
                     styleInfoSku.setPackType(styleInfoColor.getPackType());
