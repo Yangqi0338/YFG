37c762429d10a51d93da562eb493c6bd12116743
lxl <li_xianglin@126.com>
2023-09-08 19:38:56 +0800

    fix

--- src/main/java/com/base/sbc/module/purchase/service/impl/PurchaseDemandServiceImpl.java
+++ src/main/java/com/base/sbc/module/purchase/service/impl/PurchaseDemandServiceImpl.java
@@ -10,7 +10,6 @@ import cn.hutool.core.collection.CollectionUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.base.sbc.config.common.ApiResult;
 import com.base.sbc.config.common.IdGen;
-import com.base.sbc.config.common.QueryCondition;
 import com.base.sbc.config.common.base.UserCompany;
 import com.base.sbc.config.utils.BigDecimalUtil;
 import com.base.sbc.config.utils.CodeGen;
@@ -35,6 +34,7 @@ import com.base.sbc.module.purchase.service.PurchaseDemandService;
 import com.base.sbc.module.purchase.service.PurchaseOrderDetailService;
 import com.base.sbc.module.purchase.service.PurchaseOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -91,6 +91,7 @@ public class PurchaseDemandServiceImpl extends BaseServiceImpl<PurchaseDemandMap
     private PackSizeConfigService packSizeConfigService;
 
     @Autowired
+    @Lazy
     private PatternMakingService patternMakingService;
 
     @Autowired
