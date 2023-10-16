6f581665905c07d312fa7bacc3e6892490440fea
卞康 <247967116@qq.com>
2023-10-16 19:29:54 +0800

    修复bug

--- src/main/java/com/base/sbc/module/patternmaking/vo/SampleBoardVo.java
+++ src/main/java/com/base/sbc/module/patternmaking/vo/SampleBoardVo.java
@@ -257,6 +257,8 @@ public class SampleBoardVo extends Style {
     private String cuttingStatus;
     @ApiModelProperty(value = "车缝状态:0待接收,1已接收,2进行中,3完成")
     private String sewingStatus;
+    @ApiModelProperty(value = "纸样需求完成日期")
+    private String patternReqDate;
 
 
     public String getId() {
