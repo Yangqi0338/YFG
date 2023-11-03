87b8db876ec71442787320fe4c80ebb2a624796d
孟繁江 <2915350015@qq.com>
2023-11-03 17:35:23 +0800

    新增去掉-1

--- src/main/java/com/base/sbc/module/sample/dto/SaveUpdateFabricBasicInformationDto.java
+++ src/main/java/com/base/sbc/module/sample/dto/SaveUpdateFabricBasicInformationDto.java
@@ -2,11 +2,7 @@ package com.base.sbc.module.sample.dto;
 
 import com.base.sbc.module.sample.entity.FabricBasicInformation;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.hibernate.validator.constraints.NotBlank;
-
-import javax.validation.constraints.NotNull;
 
 
 @Data
