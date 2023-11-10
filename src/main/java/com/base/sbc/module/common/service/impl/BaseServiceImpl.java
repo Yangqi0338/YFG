ad66f6e87d21e13ca844a33ced96e104707e4660
卞康 <247967116@qq.com>
2023-11-10 11:38:02 +0800

    修复吊牌保存bug

--- src/main/java/com/base/sbc/module/common/service/impl/BaseServiceImpl.java
+++ src/main/java/com/base/sbc/module/common/service/impl/BaseServiceImpl.java
@@ -274,7 +274,7 @@ public class BaseServiceImpl<M extends BaseMapper<T>, T extends BaseEntity> exte
      */
     @Override
     public boolean saveOrUpdate(T entity, String name) {
-        if (this.getById(entity.getId()) != null) {
+        if (entity.getId() != null) {
             return this.updateById(entity, name);
         } else {
             return this.save(entity, name);
