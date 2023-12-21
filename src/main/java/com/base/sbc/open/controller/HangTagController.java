47b8851533648ba0439fffd704b8760cda16d352
KC <kchange0915@gmail.com>
2023-12-21 15:33:01 +0800

    fix kc 231221 配色页面排序问题

---
+++ src/main/java/com/base/sbc/open/controller/HangTagController.java
@@ -0,0 +1,95 @@
+/******************************************************************************
+ * Copyright (C) 2018 广州尚捷科技有限责任公司
+ * All Rights Reserved.
+ * 本软件为公司：广州尚捷科技有限责任公司   开发研制。未经本站正式书面同意，其他任何个人、团体
+ * 不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.base.sbc.open.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.base.sbc.client.flowable.entity.AnswerDto;
+import com.base.sbc.client.flowable.service.FlowableService;
+import com.base.sbc.config.annotation.DuplicationCheck;
+import com.base.sbc.config.common.ApiResult;
+import com.base.sbc.config.common.base.BaseController;
+import com.base.sbc.config.constant.BaseConstant;
+import com.base.sbc.config.enums.business.SystemSource;
+import com.base.sbc.config.exception.OtherException;
+import com.base.sbc.module.hangtag.dto.HangTagDTO;
+import com.base.sbc.module.hangtag.dto.HangTagMoreLanguageCheckDTO;
+import com.base.sbc.module.hangtag.dto.HangTagMoreLanguageDTO;
+import com.base.sbc.module.hangtag.dto.HangTagSearchDTO;
+import com.base.sbc.module.hangtag.dto.HangTagUpdateStatusDTO;
+import com.base.sbc.module.hangtag.dto.UpdatePriceDto;
+import com.base.sbc.module.hangtag.entity.HangTag;
+import com.base.sbc.module.hangtag.service.HangTagIngredientService;
+import com.base.sbc.module.hangtag.service.HangTagLogService;
+import com.base.sbc.module.hangtag.service.HangTagService;
+import com.base.sbc.module.hangtag.vo.HangTagListVO;
+import com.base.sbc.module.smp.SmpService;
+import com.base.sbc.module.style.entity.StyleColor;
+import com.base.sbc.module.style.service.StyleColorService;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 类描述：吊牌表 Controller类
+ *
+ * @author xhj
+ * @version 1.0
+ * @address com.base.sbc.module.hangTag.web.HangTagController
+ * @email ch.183.g1114@gmail.com
+ * @date 创建时间：2023-6-26 17:15:53
+ */
+@RestController
+@Api(tags = "吊牌表")
+@RequestMapping(value = BaseController.OPEN_URL + "/hangTag", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Validated
+@RequiredArgsConstructor
+public class HangTagController extends BaseController {
+
+    @Autowired
+    private HangTagService hangTagService;
+
+    @ApiOperation(value = "查询详情多语言")
+    @GetMapping("/getMoreLanguageDetailsByBulkStyleNo")
+    public ApiResult getMoreLanguageDetailsByBulkStyleNo(@Valid HangTagMoreLanguageDTO hangTagMoreLanguageDTO) {
+        hangTagMoreLanguageDTO.setUserCompany(super.getUserCompany());
+        return selectSuccess(hangTagService.getMoreLanguageDetailsByBulkStyleNo(hangTagMoreLanguageDTO, false));
+    }
+
+    @ApiOperation(value = "查询详情多语言")
+    @GetMapping("/getMoreLanguageCheckByBulkStyleNo")
+    public ApiResult getMoreLanguageCheckByBulkStyleNo(@Valid @RequestBody @NotEmpty(message = "检查参数列表不能为空") List<HangTagMoreLanguageCheckDTO> hangTagMoreLanguageCheckDTOList) {
+        HangTagMoreLanguageDTO hangTagMoreLanguageDTO = new HangTagMoreLanguageDTO();
+        hangTagMoreLanguageDTO.setUserCompany(super.getUserCompany());
+        hangTagMoreLanguageDTO.setHangTagMoreLanguageCheckDTOList(hangTagMoreLanguageCheckDTOList);
+        hangTagMoreLanguageDTO.setBulkStyleNo(hangTagMoreLanguageCheckDTOList.stream().map(HangTagMoreLanguageCheckDTO::getBulkStyleNo).collect(Collectors.joining(",")));
+        hangTagMoreLanguageDTO.setCountryLanguageId(hangTagMoreLanguageCheckDTOList.stream().map(HangTagMoreLanguageCheckDTO::getCountryLanguageId).collect(Collectors.joining(",")));
+        hangTagMoreLanguageDTO.setSource(hangTagMoreLanguageCheckDTOList.stream().map(HangTagMoreLanguageCheckDTO::getSource).findFirst().orElse(SystemSource.BCS));
+        return selectSuccess(hangTagService.getMoreLanguageDetailsByBulkStyleNo(hangTagMoreLanguageDTO, false));
+    }
+
+}
