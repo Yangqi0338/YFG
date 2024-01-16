81828d6f63c8258055c551cba43476fb79d4cab4
卞康 <247967116@qq.com>
2024-01-16 17:15:20 +0800

    修改订货本查询排序

--- src/main/java/com/base/sbc/module/orderbook/service/impl/OrderBookServiceImpl.java
+++ src/main/java/com/base/sbc/module/orderbook/service/impl/OrderBookServiceImpl.java
@@ -134,6 +134,7 @@ public class OrderBookServiceImpl extends BaseServiceImpl<OrderBookMapper,OrderB
         BaseQueryWrapper<OrderBook> queryWrapper=new BaseQueryWrapper<>();
         queryWrapper.notEmptyEq("tob.season_id",dto.getSeasonId());
         queryWrapper.notEmptyEq("tob.status",dto.getStatus());
+        queryWrapper.orderByDesc("id");
         return queryWrapper;
     }
 }
