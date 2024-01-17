60972c0d07ffbc614661bd3ed8924ffb021778d8
huangqiang <304570799@qq.com>
2024-01-17 15:22:29 +0800

    fix：【成衣成分送检】新建共用模块代码

---
+++ src/main/java/com/base/sbc/open/mapper/BasicsdatumGarmentInspectionMapper.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Copyright (C) 2018 广州尚捷科技有限责任公司
+ * All Rights Reserved.
+ * 本软件为公司：广州尚捷科技有限责任公司   开发研制。未经本站正式书面同意，其他任何个人、团体
+ * 不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.base.sbc.open.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import com.base.sbc.open.entity.BasicsdatumGarmentInspection;
+/** 
+ * 类描述：成衣成分送检 dao类
+ * @address com.base.sbc.open.dao.BasicsdatumGarmentInspectionDao
+ * @author your name  
+ * @email  your email
+ * @date 创建时间：2024-1-17 15:18:54 
+ * @version 1.0  
+ */
+@Mapper
+public interface BasicsdatumGarmentInspectionMapper extends BaseMapper<BasicsdatumGarmentInspection> {
+// 自定义方法区 不替换的区域【other_start】
+
+
+
+// 自定义方法区 不替换的区域【other_end】
+}
---
+++ src/main/java/com/base/sbc/open/service/impl/BasicsdatumGarmentInspectionServiceImpl.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Copyright (C) 2018 广州尚捷科技有限责任公司
+ * All Rights Reserved.
+ * 本软件为公司：广州尚捷科技有限责任公司   开发研制。未经本站正式书面同意，其他任何个人、团体
+ * 不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.base.sbc.open.service.impl;
+
+import com.base.sbc.module.common.service.impl.BaseServiceImpl;
+import com.base.sbc.open.mapper.BasicsdatumGarmentInspectionMapper;
+import com.base.sbc.open.entity.BasicsdatumGarmentInspection;
+import com.base.sbc.open.service.BasicsdatumGarmentInspectionService;
+import org.springframework.stereotype.Service;
+/** 
+ * 类描述：成衣成分送检 service类
+ * @address com.base.sbc.open.service.BasicsdatumGarmentInspectionService
+ * @author your name
+ * @email your email
+ * @date 创建时间：2024-1-17 15:18:54
+ * @version 1.0  
+ */
+@Service
+public class BasicsdatumGarmentInspectionServiceImpl extends BaseServiceImpl<BasicsdatumGarmentInspectionMapper, BasicsdatumGarmentInspection> implements BasicsdatumGarmentInspectionService {
+
+// 自定义方法区 不替换的区域【other_start】
+
+
+
+// 自定义方法区 不替换的区域【other_end】
+	
+}
