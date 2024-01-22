ecc559dcf30558f1490906b22cda6b6a4f6da712
KC <kchange0915@gmail.com>
2024-01-22 13:10:22 +0800

    feature kc 240110 订货本 修正查询

---
+++ src/main/java/com/base/sbc/module/orderbook/vo/OrderBookDetailPageVo.java
@@ -0,0 +1,23 @@
+package com.base.sbc.module.orderbook.vo;
+
+import cn.hutool.core.util.StrUtil;
+import com.base.sbc.module.orderbook.entity.OrderBookDetail;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+public class OrderBookDetailPageVo extends PageInfo<OrderBookDetailVo> {
+
+    private Map<String,Object> totalMap;
+
+    @JsonAnyGetter
+    public Map<String,Object> getTotalMap(){
+        return totalMap;
+    }
+
+}
