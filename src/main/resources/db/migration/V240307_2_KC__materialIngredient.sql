2de5219cd7cb6f5bd3c085b1e1c3e0f63311a4b6
KC <kchange0915@gmail.com>
2024-03-08 14:50:04 +0800

    feature kc 240307 修改名字

--- src/main/resources/db/migration/V240307_2_KC__materialIngredient.sql
+++
@@ -1,3 +0,0 @@
-use s_pdm_data;
-alter table t_basicsdatum_material_ingredient add column code varchar(50) default null comment '成分编码';
-alter table t_basicsdatum_material_ingredient add column say_code varchar(50) default null comment '说明编码';
