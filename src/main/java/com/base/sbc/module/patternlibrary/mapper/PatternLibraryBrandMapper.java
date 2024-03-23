4b79b1f930cfd72659cf2ccf01cff31ce239c89a
XHTE <2632964533@qq.com>
2024-03-23 10:10:21 +0800

    fix：部件库列表接口开发完成

--- src/main/java/com/base/sbc/module/patternlibrary/mapper/PatternLibraryBrandMapper.java
+++ src/main/java/com/base/sbc/module/patternlibrary/mapper/PatternLibraryBrandMapper.java
@@ -2,6 +2,7 @@ package com.base.sbc.module.patternlibrary.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.base.sbc.module.patternlibrary.entity.PatternLibraryBrand;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 版型库-品类关联表 Mapper 接口
@@ -9,6 +10,7 @@ import com.base.sbc.module.patternlibrary.entity.PatternLibraryBrand;
  * @author xhte
  * @create 2024-03-22
  */
+@Mapper
 public interface PatternLibraryBrandMapper extends BaseMapper<PatternLibraryBrand> {
 
 }
