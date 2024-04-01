7ddd2bb170e8e2b5ee63da530e27e0c8adeb40c0
ydh@qq.com <yandh0115>
2024-04-01 09:29:04 +0800

    刷新

--- src/main/java/com/base/sbc/module/hangtag/controller/HangTagController.java
+++ src/main/java/com/base/sbc/module/hangtag/controller/HangTagController.java
@@ -90,9 +90,9 @@ public class HangTagController extends BaseController {
     @ApiOperation(value = "刷新")
     @GetMapping("/getRefresh")
     @Deprecated
-    public void getRefresh(@Valid @NotBlank(message = "大货款号不可为空") String bulkStyleNo, String selectType){
-        hangTagService.getRefresh(bulkStyleNo,super.getUserCompany(),selectType);
-        return;
+    public ApiResult getRefresh(@Valid @NotBlank(message = "大货款号不可为空") String bulkStyleNo, String selectType){
+
+        return selectSuccess(hangTagService.getRefresh(bulkStyleNo,super.getUserCompany(),selectType));
     }
 
     @ApiOperation(value = "查询详情多语言")
