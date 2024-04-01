6f4800f1000ee27ecedbb4f8fdc8bac20556a6b0
KC <kchange0915@gmail.com>
2024-04-01 16:21:49 +0800

    feature kc 240401 订货本 数据库脚本

--- src/main/resources/db/migration/V6__.sql
+++ src/main/resources/db/migration/V6__.sql
@@ -2,5 +2,5 @@ alter table t_order_book_detail add column designer_distribute VARCHAR(1) defaul
 alter table t_order_book_detail add column offline_business_distribute VARCHAR(1) default '0' comment '线下商企分配';
 alter table t_order_book_detail add column online_business_distribute VARCHAR(1) default '0' comment '线上商企分配';
 
-update t_order_book_detail set hidden = '1', column_width = '80',sort_order = '9', del_flag = '0' where id = 'orderBook42';
-update t_order_book_detail set hidden = '1', column_width = '100',sort_order = '14', del_flag = '0' where id = '1742859895962791938';
+update t_column_define set hidden = '1', column_width = '80',sort_order = '9', del_flag = '0' where id = 'orderBook42';
+update t_column_define set hidden = '1', column_width = '100',sort_order = '14', del_flag = '0' where id = '1742859895962791938';
