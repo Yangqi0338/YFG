b0b6e36fe3fef7a3b6702eb8fc9ffbc8817db6c5
KC <kchange0915@gmail.com>
2024-04-03 17:24:55 +0800

    feature kc 240401 订货本 数据库脚本

--- src/main/resources/db/migration/V7__.sql
+++ src/main/resources/db/migration/V7__.sql
@@ -2,3 +2,6 @@ alter table t_country_language add column model_language_code varchar(10) defaul
 alter table t_country_language drop column country_code;
 alter table t_country_language drop column language_name;
 alter table t_country_language change column country_name name varchar(255) default null comment '名称';
+drop table t_country_model;
+drop table t_standard_column_translate;
+drop table t_style_country_print_record;
