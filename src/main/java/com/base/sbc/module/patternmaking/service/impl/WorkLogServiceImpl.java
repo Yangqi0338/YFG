e46a713c3b794fb431b2c49a215b85de751e47b9
song <641974117@qq.com>
2024-04-25 14:10:12 +0800

    fix(数据权限): 工作小账、产前样看板、坑位信息、款式设计添加虚拟部门字段

--- src/main/java/com/base/sbc/module/patternmaking/service/impl/WorkLogServiceImpl.java
+++ src/main/java/com/base/sbc/module/patternmaking/service/impl/WorkLogServiceImpl.java
@@ -11,12 +11,15 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import com.base.sbc.client.amc.enums.DataPermissionsBusinessTypeEnum;
+import com.base.sbc.client.amc.service.AmcService;
 import com.base.sbc.client.amc.service.DataPermissionsService;
+import com.base.sbc.config.common.ApiResult;
 import com.base.sbc.config.common.BaseQueryWrapper;
 import com.base.sbc.config.redis.RedisUtils;
 import com.base.sbc.config.utils.CopyUtil;
 import com.base.sbc.config.utils.ExcelUtils;
 import com.base.sbc.config.utils.QueryGenerator;
+import com.base.sbc.module.common.dto.VirtualDept;
 import com.base.sbc.module.common.service.impl.BaseServiceImpl;
 import com.base.sbc.module.patternmaking.dto.WorkLogSaveDto;
 import com.base.sbc.module.patternmaking.dto.WorkLogSearchDto;
@@ -35,6 +38,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 类描述：工作小账 service类
@@ -54,6 +58,8 @@ public class WorkLogServiceImpl extends BaseServiceImpl<WorkLogMapper, WorkLog> 
     RedisUtils redisUtils;
     @Autowired
     private DataPermissionsService dataPermissionsService;
+    @Autowired
+    private AmcService amcService;
 
 
     @Override
@@ -85,6 +91,13 @@ public class WorkLogServiceImpl extends BaseServiceImpl<WorkLogMapper, WorkLog> 
         long maxNo = NumberUtil.max(dbMaxNo, defMaxNo);
         String code = String.format("LS%010d", ++maxNo);
         bean.setCode(code);
+
+        //保存创建人的虚拟部门
+        ApiResult<List<VirtualDept>> virtualDeptByUserId = amcService.getVirtualDeptByUserId(getUserId());
+        List<VirtualDept> data = virtualDeptByUserId.getData();
+        String ids = data.stream().map(VirtualDept::getId).collect(Collectors.joining());
+        bean.setCreateDeptId(ids);
+
         this.save(bean, "工作小账");
         return BeanUtil.copyProperties(workLog, WorkLogVo.class);
     }
