c6838a201183c4786d89dc4a1f0e55debcece524
song <641974117@qq.com>
2024-04-28 15:15:54 +0800

    fix(数据权限): 企划管理-产品季、渠道查询权限

--- src/main/java/com/base/sbc/module/planning/service/impl/PlanningChannelServiceImpl.java
+++ src/main/java/com/base/sbc/module/planning/service/impl/PlanningChannelServiceImpl.java
@@ -40,10 +40,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -156,7 +153,7 @@ public class PlanningChannelServiceImpl extends BaseServiceImpl<PlanningChannelM
         qw.notEmptyEq("c.channel", dto.getChannel());
         qw.notEmptyLike("c.sex", dto.getSex());
         qw.orderByDesc("id");
-        dataPermissionsService.getDataPermissionsForQw(qw, DataPermissionsBusinessTypeEnum.PlanningChannel.getK(), "s.");
+        dataPermissionsService.getDataPermissionsForQw(qw, DataPermissionsBusinessTypeEnum.PlanningChannel.getK());
         Page<PlanningChannelVo> page = PageHelper.startPage(dto);
         List<PlanningChannelVo> list = getBaseMapper().list(qw);
         PageInfo<PlanningChannelVo> pageInfo = page.toPageInfo();
@@ -196,8 +193,8 @@ public class PlanningChannelServiceImpl extends BaseServiceImpl<PlanningChannelM
         qw.groupBy("planning_season_id");
 
         Map<String, Long> result = new HashMap<>(16);
-        List<Map<String, Object>> listMap = getBaseMapper().selectMaps(qw);
-        List<CountVo> list = BeanUtil.copyToList(listMap, CountVo.class);
+        dataPermissionsService.getDataPermissionsForQw(qw, DataPermissionsBusinessTypeEnum.PlanningChannel.getK());
+        List<CountVo> list = getBaseMapper().selectIdCount(qw);
         if (CollUtil.isNotEmpty(list)) {
             for (CountVo countVo : list) {
                 result.put(countVo.getLabel(), countVo.getCount());
