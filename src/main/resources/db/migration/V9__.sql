03833662a60e62bf5ea4cc8c7883aeeeb209d166
KC <kchange0915@gmail.com>
2024-04-29 16:13:54 +0800

    :sparkles: feature kc 240407 订货本3.1 修改sql

--- src/main/resources/db/migration/V9__.sql
+++ src/main/resources/db/migration/V9__.sql
@@ -7,16 +7,16 @@ alter table t_order_book_detail add column order_person varchar(50) default null
 alter table t_order_book_detail add column order_person_name varchar(50) default null comment '下单人名字';
 alter table t_order_book_detail add column order_date datetime default null comment '下单时间';
 alter table t_order_book_detail add column order_no varchar(50) default null comment '投产编码';
-alter table t_order_book_detail add column order_send_status varchar(2) default null comment '投产下发状态';
+alter table t_order_book_detail add column order_send_status varchar(5) default null comment '投产下发状态';
 alter table t_order_book_detail add column order_send_warn_msg varchar(500) default null comment '投产下发错误信息';
 alter table t_order_book_detail add column delivery_at datetime default null comment '商品要求货期';
-alter table t_order_book_detail add column sale_type_id varchar(10) default null comment '销售分类';
+alter table t_order_book_detail add column sale_type_id varchar(20) default null comment '销售分类id';
+alter table t_order_book_detail add column sale_type varchar(20) default null comment '销售分类';
 alter table t_order_book_detail add column budget_no varchar(50) default null comment '预算号';
 alter table t_order_book_detail add column budget_no_id varchar(50) default null comment '预算号id';
 alter table t_order_book_detail add column fac_merge varchar(2) default null comment '合并投产';
-alter table t_order_book_detail add column place_order_type varchar(2) default null comment '下单类型';
+alter table t_order_book_detail add column place_order_type varchar(10) default null comment '下单类型';
 alter table t_order_book_detail add column devt_type varchar(10) default null comment '投产类型';
 alter table t_order_book_detail modify column unit_dosage_ids varchar(500) default null comment '单件用量/里';
 alter table t_order_book_detail modify column unit_fabric_dosage_ids varchar(500) default null comment '单件用量/里';
-
-
+alter table t_push_records modify column response_message text default null comment '返回消息，推送结果的返回消息或错误信息';
