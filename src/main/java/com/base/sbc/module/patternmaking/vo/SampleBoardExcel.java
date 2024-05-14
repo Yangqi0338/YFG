ec80935479de2604268d1e8da83eb94b43ba35ef
huangqiang <huangqiang@qq.com>
2024-05-14 17:50:20 +0800

    feat：【样衣看板】缺少是否参考样衣字段

--- src/main/java/com/base/sbc/module/patternmaking/vo/SampleBoardExcel.java
+++ src/main/java/com/base/sbc/module/patternmaking/vo/SampleBoardExcel.java
@@ -137,6 +137,13 @@ public class SampleBoardExcel {
     @Excel(name = "需求数",type = 10)
     private BigDecimal requirementNum;
 
+    /**
+     * 是否参考样衣
+     */
+    @ApiModelProperty(value = "是否参考样衣")
+    @Excel(name = "是否参考样衣",type = 11)
+    private String referSample;
+
 
 
     @ApiModelProperty(value = "纸样完成时间")
