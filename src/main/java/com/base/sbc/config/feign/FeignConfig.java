53c1b08c59932cb60c36fbc4992715eef56133ee
KC <kchange0915@gmail.com>
2024-05-21 12:29:39 +0800

    fix(全局): feign 请求头连接报错

--- src/main/java/com/base/sbc/config/feign/FeignConfig.java
+++ src/main/java/com/base/sbc/config/feign/FeignConfig.java
@@ -32,6 +32,9 @@ public class FeignConfig {
                     if (headerNames != null) {
                         while (headerNames.hasMoreElements()) {
                             String name = headerNames.nextElement();
+                            if ("content-length".equals(name)) {
+                                continue;
+                            }
                             if(!"Content-type".equalsIgnoreCase(name)){
                                 String values = request.getHeader(name);
                                 requestTemplate.header(name, values);
