2078ad83914ddad2ac99f1ee069bb4f42e0f31ad
song <641974117@qq.com>
2024-05-31 13:54:25 +0800

    fix(款式配色): 下发下游系统时，动态字段配置

---
+++ src/main/java/com/base/sbc/module/formtype/controller/FieldBusinessSystemController.java
@@ -0,0 +1,94 @@
+/******************************************************************************
+ * Copyright (C) 2018 广州尚捷科技有限责任公司
+ * All Rights Reserved.
+ * 本软件为公司：广州尚捷科技有限责任公司   开发研制。未经本站正式书面同意，其他任何个人、团体
+ * 不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.base.sbc.module.formtype.controller;
+
+import com.base.sbc.config.common.ApiResult;
+import com.base.sbc.config.common.base.BaseController;
+import com.base.sbc.module.formtype.dto.FieldBusinessSystemQueryDto;
+import com.base.sbc.module.formtype.entity.FieldBusinessSystem;
+import com.base.sbc.module.formtype.service.FieldBusinessSystemService;
+import com.base.sbc.module.formtype.vo.FieldBusinessSystemVo;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 类描述：字段对应下游系统关系 Controller类
+ *
+ * @author your name
+ * @version 1.0
+ * @address com.base.sbc.module.formtype.web.FieldBusinessSystemController
+ * @email your email
+ * @date 创建时间：2024-5-31 10:35:28
+ */
+@RestController
+@Api(tags = "字段对应下游系统关系")
+@RequestMapping(value = BaseController.SAAS_URL + "/fieldBusinessSystem", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Validated
+public class FieldBusinessSystemController {
+
+    @Autowired
+    private FieldBusinessSystemService fieldBusinessSystemService;
+
+    @ApiOperation(value = "分页查询")
+    @GetMapping
+    public PageInfo<FieldBusinessSystemVo> page(FieldBusinessSystemQueryDto dto) {
+        return fieldBusinessSystemService.findPage(dto);
+    }
+
+    @ApiOperation(value = "删除-通过id查询,多个逗号分开")
+    @DeleteMapping("/del}")
+    public ApiResult removeById(@RequestBody List<String> list) {
+        fieldBusinessSystemService.removeByIds(list);
+        return ApiResult.success("删除成功");
+    }
+
+    @ApiOperation(value = "保存")
+    @PostMapping
+    public ApiResult save(@RequestBody List<FieldBusinessSystem> list) {
+        fieldBusinessSystemService.saveBatch(list);
+        return ApiResult.success("保存成功");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/base/sbc/module/formtype/service/impl/FieldBusinessSystemServiceImpl.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Copyright (C) 2018 广州尚捷科技有限责任公司
+ * All Rights Reserved.
+ * 本软件为公司：广州尚捷科技有限责任公司   开发研制。未经本站正式书面同意，其他任何个人、团体
+ * 不得使用、复制、修改或发布本软件.
+ *****************************************************************************/
+package com.base.sbc.module.formtype.service.impl;
+
+import com.base.sbc.config.common.BaseQueryWrapper;
+import com.base.sbc.module.common.service.impl.BaseServiceImpl;
+import com.base.sbc.module.formtype.dto.FieldBusinessSystemQueryDto;
+import com.base.sbc.module.formtype.entity.FieldBusinessSystem;
+import com.base.sbc.module.formtype.mapper.FieldBusinessSystemMapper;
+import com.base.sbc.module.formtype.service.FieldBusinessSystemService;
+import com.base.sbc.module.formtype.vo.FieldBusinessSystemVo;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 类描述：字段对应下游系统关系 service类
+ *
+ * @author your name
+ * @version 1.0
+ * @address com.base.sbc.module.formtype.service.FieldBusinessSystemService
+ * @email your email
+ * @date 创建时间：2024-5-31 10:35:28
+ */
+@Service
+public class FieldBusinessSystemServiceImpl extends BaseServiceImpl<FieldBusinessSystemMapper, FieldBusinessSystem> implements FieldBusinessSystemService {
+
+
+    @Override
+    public PageInfo<FieldBusinessSystemVo> findPage(FieldBusinessSystemQueryDto dto) {
+        Page<Object> objects = PageHelper.startPage(dto);
+        BaseQueryWrapper<FieldBusinessSystem> qw = new BaseQueryWrapper<>();
+        qw.notEmptyEq("tfbs.business_type", dto.getBusinessType());
+        List<FieldBusinessSystemVo> list = baseMapper.findPage(qw);
+        return new PageInfo<>(list);
+    }
+
+
+}
