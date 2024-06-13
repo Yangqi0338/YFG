30e965eb7e7e8e3a0ebef3040570ad0d1331f8e6
yase <yase@tsign.cn>
2024-06-13 14:23:22 +0800

    feat 批量修改素材url过长问题

--- src/main/java/com/base/sbc/module/material/service/impl/MaterialServiceImpl.java
+++ src/main/java/com/base/sbc/module/material/service/impl/MaterialServiceImpl.java
@@ -345,6 +345,7 @@ public class MaterialServiceImpl extends BaseServiceImpl<MaterialMapper, Materia
             QueryWrapper<MaterialLabel> labelQueryWrapper = new QueryWrapper<>();
             labelQueryWrapper.eq("material_id", materialSaveDto.getId());
             materialLabelService.addAndUpdateAndDelList(materialSaveDto.getLabels(), labelQueryWrapper);
+            materialSaveDto.setPicUrl(CommonUtils.removeQuery(material.getPicUrl()));
             this.updateById(materialSaveDto);
         }
 
