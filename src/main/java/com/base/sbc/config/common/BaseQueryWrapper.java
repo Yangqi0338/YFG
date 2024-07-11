abe77bbc153a5d953d209795f7d48c4850c12eed
KC <kchange0915@gmail.com>
2024-07-11 15:56:28 +0800

    ⚡️ perf(款式定价): 查询优化 修正二版

--- src/main/java/com/base/sbc/config/common/BaseQueryWrapper.java
+++ src/main/java/com/base/sbc/config/common/BaseQueryWrapper.java
@@ -1,24 +1,32 @@
 package com.base.sbc.config.common;
 
-import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.CharUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.ISqlSegment;
 import com.baomidou.mybatisplus.core.conditions.SharedString;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
-import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.core.conditions.segments.NormalSegmentList;
+import com.baomidou.mybatisplus.core.enums.SqlKeyword;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.base.sbc.config.common.base.BaseGlobal;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.util.StringUtils;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 /**
  * @author 卞康
@@ -170,7 +178,8 @@ public class BaseQueryWrapper<T> extends QueryWrapper<T> {
         this.and(wrapper -> {
             wrapper.and(i -> {
                 for (int j = 0; j < val.size(); j++) {
-                    i.like(columns, val.get(j)).or(j < val.size() - 1);
+                    String value = val.get(j);
+                    i.like(StrUtil.isNotBlank(value), columns, value).or(j < val.size() - 1);
                 }
             });
         });
@@ -233,8 +242,69 @@ public class BaseQueryWrapper<T> extends QueryWrapper<T> {
         return this;
     }
 
-//    @Override
-//    public BaseLambdaQueryWrapper<T> lambda() {
-//        return BeanUtil.copyProperties(super.lambda(), BaseLambdaQueryWrapper.class );
-//    }
+    /** 适配count */
+    public Map<String, String> getGroupSqlSegment() {
+        Map<String, StringJoiner> map = new HashMap<>();
+        MergeSegments expression = getExpression();
+        List<ISqlSegment> linkKeyword = Arrays.asList(SqlKeyword.AND, SqlKeyword.OR);
+        if (Objects.nonNull(expression)) {
+            NormalSegmentList normal = expression.getNormal();
+            // 遍历
+            StringJoiner joiner = new StringJoiner(StringPool.SPACE);
+            AtomicInteger handlerNum = new AtomicInteger(0);
+            for (int i = 0, normalSize = normal.size() - 1; i <= normalSize; i++) {
+                boolean lastElement = i == normalSize;
+                int num = handlerNum.get();
+                boolean nonFinish = IntStream.of(1, 2).anyMatch(it -> it == num);
+                boolean isFinish = num > 2;
+                ISqlSegment iSqlSegment = normal.get(i);
+                String sqlSegment = iSqlSegment.getSqlSegment();
+                // 支持嵌套 TODO
+                if (iSqlSegment instanceof QueryWrapper) {
+                    continue;
+                }
+                if (linkKeyword.contains(iSqlSegment) && isFinish) {
+                    if (iSqlSegment == SqlKeyword.AND) {
+                        // 若下一个的前缀和之前一
+                        if ((i + 1) != normalSize) {
+                            String[] split = normal.get(i + 1).getSqlSegment().split(Pattern.quote(StringPool.DOT));
+                            // 有前缀
+                            String key = split[0];
+                            if (split.length > 1 && map.containsKey(key)) {
+                                map.get(key).add(sqlSegment);
+                                handlerNum.set(0);
+                            }
+                        }
+                        continue;
+                    }
+                    // 支持OR TODO
+//                    if (iSqlSegment == SqlKeyword.OR)
+                }
+                // 找 ColumnSegment, 直到下一个为止
+                if (!nonFinish && !lastElement && normal.get(i + 1) instanceof SqlKeyword && !linkKeyword.contains(iSqlSegment)) {
+                    String[] split = sqlSegment.split(Pattern.quote(StringPool.DOT));
+                    String key = " ";
+                    // 有前缀
+                    if (split.length > 1) {
+                        key = split[0];
+                    }
+                    joiner = map.getOrDefault(key, new StringJoiner(StringPool.SPACE));
+                    joiner.add(sqlSegment);
+                    map.put(key, joiner);
+                    handlerNum.set(1);
+                } else {
+                    joiner.add(sqlSegment);
+                    handlerNum.incrementAndGet();
+                }
+            }
+
+        }
+        map.remove(" ");
+        Map<String, String> result = MapUtil.map(map, (key, value) -> {
+            if (value.length() <= 2) return null;
+            return value.toString();
+        });
+        return MapUtil.removeNullValue(result);
+    }
+
 }
