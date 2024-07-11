07c574eafe916ca75aa1a09a26f20d9121f1f186
song <641974117@qq.com>
2024-07-11 14:33:52 +0800

    fix(fob): 样衣审样看板

--- src/main/java/com/base/sbc/module/patternmaking/vo/PatternMakingBarCodeVo.java
+++ src/main/java/com/base/sbc/module/patternmaking/vo/PatternMakingBarCodeVo.java
@@ -9,35 +9,45 @@ package com.base.sbc.module.patternmaking.vo;
 import com.base.sbc.module.patternmaking.entity.PatternMakingBarCode;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
+
+import java.util.Date;
+
 /**
  * 类描述：Vo 实体类
- * @address com.base.sbc.module.patternmaking.vo.PatternMakingBarCodeVo
+ *
  * @author your name
+ * @version 1.0
+ * @address com.base.sbc.module.patternmaking.vo.PatternMakingBarCodeVo
  * @email your email
  * @date 创建时间：2024-7-3 15:56:26
- * @version 1.0
  */
 @Data
 @ApiModel(" PatternMakingBarCodeVo")
 public class PatternMakingBarCodeVo extends PatternMakingBarCode {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String designNo;
-	private String patternNo;
-	private String sampleType;
-	private String sampleTypeName;
-	private String patSeqName;
-	private String yearName;
-	private String seasonName;
-	private String brandName;
-	private String requirementNum;
-	private String styleNo;
-	private String colorName;
-	private String styleColorPic;
-	private String stylePic;
+    private String designNo;
+    private String patternNo;
+    private String sampleType;
+    private String sampleTypeName;
+    private String patSeqName;
+    private String yearName;
+    private String seasonName;
+    private String brandName;
+    private String requirementNum;
+    private String styleNo;
+    private String colorName;
+    private String styleColorPic;
+    private String stylePic;
+    private String patternRoom;
+    private Date techReceiveTime;
+    private String status;
+    private String techRemarks;
+    private Date processDepartmentDate;
+    private String supplierName;
 
-	public String getSeason(){
-		return yearName+"-"+seasonName+"-"+brandName;
-	}
+    public String getSeason() {
+        return yearName + "-" + seasonName + "-" + brandName;
+    }
 }
