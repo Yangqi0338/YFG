04441c98b405a35acce0226ea6c24dc85936c12b
huangqiang <huangqiang@qq.com>
2024-07-15 13:28:21 +0800

    fix：【MANGO吊牌导入】去掉水洗唛校验

--- src/main/java/com/base/sbc/module/style/service/impl/StyleColorServiceImpl.java
+++ src/main/java/com/base/sbc/module/style/service/impl/StyleColorServiceImpl.java
@@ -3645,7 +3645,7 @@ public class StyleColorServiceImpl<pricingTemplateService> extends BaseServiceIm
                 updateHangTag.setWashingCode(washIconsisExistList.get(0).getCode());
                 updateHangTag.setWashingLabel(washIconsisExistList.get(0).getUrl());
             }else{
-                // 暂时去掉水洗唛校验问题
+                // 暂时去掉水洗唛校验问题20240715
                 //errorInfo+="第" + (i + 1) + "行,【" + washingCode + "】找不到对应的洗标信息！\n";
             }
             //endregion
