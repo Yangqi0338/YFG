e7f373926767e13f88dd04571862841bf6258ab7
js <291178013@qq.com>
2024-07-16 15:02:51 +0800

    feat 新增发布人字段

--- src/main/java/com/base/sbc/module/material/dto/MaterialQueryDto.java
+++ src/main/java/com/base/sbc/module/material/dto/MaterialQueryDto.java
@@ -132,4 +132,7 @@ public class MaterialQueryDto extends Page {
 
     @ApiModelProperty(value = "发布人名称")
     private String issuerName;
+
+    @ApiModelProperty(value = "发布人工号")
+    private String issuerUsername;
 }
