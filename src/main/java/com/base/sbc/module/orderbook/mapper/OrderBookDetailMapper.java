5421b75ffe502c657b4f174a2b5da0e2b60d49a0
KC <kchange0915@gmail.com>
2024-07-25 11:02:39 +0800

    🔀 merged(冲突): 代码合并

--- src/main/java/com/base/sbc/module/orderbook/mapper/OrderBookDetailMapper.java
+++ src/main/java/com/base/sbc/module/orderbook/mapper/OrderBookDetailMapper.java
@@ -3,6 +3,7 @@ package com.base.sbc.module.orderbook.mapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.base.sbc.config.common.BaseQueryWrapper;
 import com.base.sbc.module.common.mapper.BaseEnhanceMapper;
+import com.base.sbc.module.orderbook.dto.OrderBookDetailQueryDto;
 import com.base.sbc.module.orderbook.dto.QueryOrderDetailDTO;
 import com.base.sbc.module.orderbook.entity.OrderBookDetail;
 import com.base.sbc.module.orderbook.vo.OrderBookDetailForSeasonPlanningVO;
