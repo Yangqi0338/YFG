fd39425293d5000dc1885fe2f48f42370346d8d5
song <641974117@qq.com>
2024-08-01 13:51:29 +0800

    fix(fob): 状态查询调整

--- src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
+++ src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
@@ -882,9 +882,10 @@ public class PatternMakingServiceImpl extends BaseServiceImpl<PatternMakingMappe
                 qw.isNullStr("tpmbc.bar_code");
             }else if (dto.getStatus().equals("已收样")){
                 qw.isNotNullStr("tpmbc.bar_code");
+                qw.eq("tpmbc.status", "0");
             }else if (dto.getStatus().equals("已审样")){
                 qw.isNotNullStr("tpmbc.bar_code");
-                qw.notEmptyIn("tpmbc.status", Arrays.asList("1","2"));
+                qw.in("tpmbc.status", Arrays.asList("1","2"));
             }
             list = getBaseMapper().patternMakingTaskFOBList(qw);
         } else {
