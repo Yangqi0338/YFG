ef7c8377197fb0ecc239ebe28c22ad801bf9d601
XHTE <2632964533@qq.com>
2024-08-06 09:40:25 +0800

    feat(款式配色): 生产类型逻辑问题修改
    1.大货同步下发的时候取大货款的生产类型

--- src/main/java/com/base/sbc/module/smp/SmpService.java
+++ src/main/java/com/base/sbc/module/smp/SmpService.java
@@ -484,8 +484,8 @@ public class SmpService {
             }
 
             //生产类型
-            smpGoodsDto.setProductionType(style.getDevtType());
-            smpGoodsDto.setProductionTypeName(style.getDevtTypeName());
+            smpGoodsDto.setProductionType(styleColor.getDevtType().getCode());
+            smpGoodsDto.setProductionTypeName(styleColor.getDevtTypeName());
             smpGoodsDto.setBandName(style.getBandName());
             smpGoodsDto.setAccessories("配饰".equals(style.getProdCategory1stName()));
 
