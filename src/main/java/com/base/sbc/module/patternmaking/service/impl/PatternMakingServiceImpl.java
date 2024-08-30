d415329c96f49aa54ef3cfd97ec4ece6e83bc5cc
KC <kchange0915@gmail.com>
2024-08-30 15:53:56 +0800

    🐛 fix(版房指令): 判断逻辑

--- src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
+++ src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
@@ -236,10 +236,12 @@ public class PatternMakingServiceImpl extends BaseServiceImpl<PatternMakingMappe
         QueryWrapper<PatternMaking> qw = new QueryWrapper<>();
         qw.eq("m.style_id", styleId);
         qw.eq("m.del_flag", BaseGlobal.NO);
-        if (StrUtil.equals(ProductionType.FOB.getCode(), patternMakingDevtType)) {
-            qw.eq("m.pattern_making_devt_type", patternMakingDevtType);
-        } else {
-            qw.and(item -> item.eq("m.pattern_making_devt_type", patternMakingDevtType).or().isNull("m.pattern_making_devt_type"));
+        if (StrUtil.isNotBlank(patternMakingDevtType)) {
+            if (StrUtil.equals(ProductionType.FOB.getCode(), patternMakingDevtType)) {
+                qw.eq("m.pattern_making_devt_type", patternMakingDevtType);
+            } else {
+                qw.and(item -> item.eq("m.pattern_making_devt_type", patternMakingDevtType));
+            }
         }
         qw.orderBy(true, true , "create_date");
         List<PatternMakingListVo> patternMakingListVos = getBaseMapper().findBySampleDesignId(qw);
