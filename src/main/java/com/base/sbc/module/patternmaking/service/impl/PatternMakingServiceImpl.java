fac046147eb13ed1f4fbef2d74fbc953a7eb5480
song <641974117@qq.com>
2024-09-02 10:12:55 +0800

    fix(fob): 样衣看板，产前样看板图片解密处理

--- src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
+++ src/main/java/com/base/sbc/module/patternmaking/service/impl/PatternMakingServiceImpl.java
@@ -1815,6 +1815,7 @@ public class PatternMakingServiceImpl extends BaseServiceImpl<PatternMakingMappe
                     SampleBoardVo::setRatingProdCategory, SampleBoardVo::setRatingDetailDTO, SampleBoardVo::setRatingConfigList);
         }
 
+        uploadFileService.setObjectUrlToList(objects.toPageInfo().getList(),"samplePicUrl","samplePicUrl1","samplePicUrl2","samplePicUrl3","samplePicUrl4", "sampleVideoUrl", "sampleUrl");
         PatternMakingCommonPageSearchVo pageVo = BeanUtil.copyProperties(objects.toPageInfo(),PatternMakingCommonPageSearchVo.class);
         pageVo.setPatternMakingScoreVo(sampleBoardScore(qw));
 
